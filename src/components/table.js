600e5f82efa5458dccce83625063a2a40ef27493
KurtZhou <pandabat1@gmail.com>
2019-10-01 12:07:40 +0800

    縮行150

--- src/components/table.js
+++ src/components/table.js
@@ -42,82 +42,16 @@ class Table extends Component {
   };
 
   clickRow = j => {
-    if (j === 0) {
-      this.setState({
-        click: [true, false, false, false, false, false, false]
-      });
-    } else if (j === 1) {
-      this.setState({
-        click: [false, true, false, false, false, false, false]
-      });
-    } else if (j === 2) {
-      this.setState({
-        click: [false, false, true, false, false, false, false]
-      });
-    } else if (j === 3) {
-      this.setState({
-        click: [false, false, false, true, false, false, false]
-      });
-    } else if (j === 4) {
-      this.setState({
-        click: [false, false, false, false, true, false, false]
-      });
-    } else if (j === 5) {
-      this.setState({
-        click: [false, false, false, false, false, true, false]
-      });
-    } else if (j === 6) {
-      this.setState({
-        click: [false, false, false, false, false, false, true]
-      });
-    }
+    this.setState({
+      click: { [j]: true }
+    });
   };
 
   clickColumn = i => {
-    if (i === 0) {
-      this.setState({
-        test: [true, false, false, false, false, false, false]
-      });
-    } else if (i === 1) {
-      this.setState({
-        test: [false, true, false, false, false, false, false]
-      });
-    } else if (i === 2) {
-      this.setState({
-        test: [false, false, true, false, false, false, false]
-      });
-    } else if (i === 3) {
-      this.setState({
-        test: [false, false, false, true, false, false, false]
-      });
-    } else if (i === 4) {
-      this.setState({
-        test: [false, false, false, false, true, false, false]
-      });
-    } else if (i === 5) {
-      this.setState({
-        test: [false, false, false, false, false, true, false]
-      });
-    } else if (i === 6) {
-      this.setState({
-        test: [false, false, false, false, false, false, true]
-      });
-      console.log("it's test");
-    }
+    this.setState({
+      test: { [i]: true }
+    });
   };
-
-  // cheapest = () => {
-  //   // for (let i = 0; i < 7; i++) {
-  //   let new_array = JsonData[0].detail.map(value => {
-  //     console.log("AAA", value.cheapest);
-  //     if (value.cheapest) {
-  //       this.setState({
-  //         cheapest: "cheapest"
-  //       });
-  //     }
-  //   });
-  //   // }
-  // };
 
   render() {
     const { bannerNameL, bannerNameR, column, cheapest } = this.state;
@@ -145,19 +79,19 @@ class Table extends Component {
             <i className="icon ic-ln"></i>
           </div>
           <table className="table">
-            <tr>
-              <td>
-                <div className="title2">
-                  <span>回程</span>
-                  <span>去程</span>
-                </div>
-              </td>
-              <div className={`slide clearfix date2 ${column}`}>
-                {JsonData[0].detail.map((ele, i) => {
-                  {
-                  }
-                  return (
-                    <td>
+            <tbody>
+              <tr>
+                <td>
+                  <div className="title2">
+                    <span>回程</span>
+                    <span>去程</span>
+                  </div>
+                </td>
+                <td className={`slide clearfix date2 ${column}`}>
+                  {JsonData[0].detail.map((ele, i) => {
+                    {
+                    }
+                    return (
                       <div className={`col col${i + 1}`}>
                         <span>{ele.backDate}</span>
                         <div
@@ -168,253 +102,45 @@ class Table extends Component {
                           2018
                         </div>
                       </div>
-                    </td>
-                  );
-                })}
-              </div>
-            </tr>
-            <tr>
-              <td>
-                <div className="date">
-                  <span>12/27(三)</span>
-                </div>
-              </td>
-              <div className={`slide clearfix ${column}`}>
-                {JsonData[0].detail.map((ele, i) => {
-                  {
-                    /* JsonData[0].detail */
-                  }
-                  // {JsonData.detail[0].price.map((ele, i) => {
-                  return (
+                    );
+                  })}
+                </td>
+              </tr>
+              {JsonData.map((ele, k) => {
+                return (
+                  <tr>
                     <td>
-                      <div
-                        className={`col col${i + 1} ${
-                          this.state.click[0] ? "open1" : null
-                        } ${this.state.test[i] ? "open2" : null}`}
-                        onClick={() => {
-                          this.clickRow(0);
-                          this.clickColumn(i);
-                        }}
-                      >
-                        <div
-                          className={`${ele.cheapest ? "cheapest" : ""}`}
-                        ></div>
-                        <span>{ele.price}</span>
+                      <div className="date">
+                        <span>{ele.goDate}</span>
                       </div>
                     </td>
-                  );
-                })}
-              </div>
-            </tr>
-            <tr>
-              <td>
-                <div className="date">
-                  <span>12/28(四)</span>
-                </div>
-              </td>
-              <div className={`slide clearfix ${column}`}>
-                {JsonData[1].detail.map((ele, i) => {
-                  {
-                    /* JsonData[0].detail */
-                  }
-                  // {JsonData.detail[0].price.map((ele, i) => {
-                  return (
-                    <td>
-                      <div
-                        className={`col col${i + 1} ${
-                          this.state.click[1] ? "open1" : null
-                        } ${this.state.test[i] ? "open2" : null}`}
-                        onClick={() => {
-                          this.clickRow(1);
-                          this.clickColumn(i);
-                        }}
-                      >
-                        <div
-                          className={`${ele.cheapest ? "cheapest" : ""}`}
-                        ></div>
-                        <div
-                          className={`${ele.cheapest ? "cheapestw" : ""}`}
-                        >{`${ele.cheapest ? "最便宜" : ""}`}</div>
-                        <span>{ele.price}</span>
-                      </div>
+                    <td className={`slide clearfix ${column}`}>
+                      {JsonData[k].detail.map((ele, i) => {
+                        return (
+                          <div
+                            className={`col col${i + 1} ${
+                              this.state.click[k] ? "open1" : null
+                            } ${this.state.test[i] ? "open2" : null}`}
+                            onClick={() => {
+                              this.clickRow(k);
+                              this.clickColumn(i);
+                            }}
+                          >
+                            <div
+                              className={`${ele.cheapest ? "cheapest" : ""}`}
+                            ></div>
+                            <div
+                              className={`${ele.cheapest ? "cheapestw" : ""}`}
+                            >{`${ele.cheapest ? "最便宜" : ""}`}</div>
+                            <span>{ele.price}</span>
+                          </div>
+                        );
+                      })}
                     </td>
-                  );
-                })}
-              </div>
-            </tr>
-            <tr>
-              <td>
-                <div className="date">
-                  <span>12/29(五)</span>
-                </div>
-              </td>
-
-              <div className={`slide clearfix ${column}`}>
-                {JsonData[2].detail.map((ele, i) => {
-                  {
-                    /* JsonData[0].detail */
-                  }
-                  // {JsonData.detail[0].price.map((ele, i) => {
-                  return (
-                    <td>
-                      <div
-                        className={`col col${i + 1} ${
-                          this.state.click[2] ? "open1" : null
-                        } ${this.state.test[i] ? "open2" : null}`}
-                        onClick={() => {
-                          this.clickRow(2);
-                          this.clickColumn(i);
-                        }}
-                      >
-                        <div
-                          className={`${ele.cheapest ? "cheapest" : ""}`}
-                        ></div>
-                        <span>{ele.price}</span>
-                      </div>
-                    </td>
-                  );
-                })}
-              </div>
-            </tr>
-            <tr>
-              <td>
-                <div className="date">
-                  <span>12/30(六)</span>
-                </div>
-              </td>
-              <div className={`slide clearfix ${column}`}>
-                {JsonData[3].detail.map((ele, i) => {
-                  {
-                    /* JsonData[0].detail */
-                  }
-                  // {JsonData.detail[0].price.map((ele, i) => {
-                  return (
-                    <td>
-                      <div
-                        className={`col col${i + 1} ${
-                          this.state.click[3] ? "open1" : null
-                        } ${this.state.test[i] ? "open2" : null}`}
-                        onClick={() => {
-                          this.clickRow(3);
-                          this.clickColumn(i);
-                        }}
-                      >
-                        <div
-                          className={`${ele.cheapest ? "cheapest" : ""}`}
-                        ></div>
-                        <div
-                          className={`${ele.cheapest ? "cheapestw" : ""}`}
-                        >{`${ele.cheapest ? "最便宜" : ""}`}</div>
-                        <span>{ele.price}</span>
-                      </div>
-                    </td>
-                  );
-                })}
-              </div>
-            </tr>
-            <tr>
-              <td>
-                <div className="date">
-                  <span>12/31(日)</span>
-                </div>
-              </td>
-              <div className={`slide clearfix ${column}`}>
-                {JsonData[4].detail.map((ele, i) => {
-                  {
-                    /* JsonData[0].detail */
-                  }
-                  // {JsonData.detail[0].price.map((ele, i) => {
-                  return (
-                    <td>
-                      <div
-                        className={`col col${i + 1} ${
-                          this.state.click[4] ? "open1" : null
-                        } ${this.state.test[i] ? "open2" : null}`}
-                        onClick={() => {
-                          this.clickRow(4);
-                          this.clickColumn(i);
-                        }}
-                      >
-                        <div
-                          className={`${ele.cheapest ? "cheapest" : ""}`}
-                        ></div>
-                        <div
-                          className={`${ele.cheapest ? "cheapestw" : ""}`}
-                        >{`${ele.cheapest ? "最便宜" : ""}`}</div>
-                        <span>{ele.price}</span>
-                      </div>
-                    </td>
-                  );
-                })}
-              </div>
-            </tr>
-            <tr>
-              <td>
-                <div className="date">
-                  <span>01/01(一)</span>
-                  <div className="year">2018</div>
-                </div>
-              </td>
-              <div className={`slide clearfix ${column}`}>
-                {JsonData[5].detail.map((ele, i) => {
-                  {
-                    /* JsonData[0].detail */
-                  }
-                  // {JsonData.detail[0].price.map((ele, i) => {
-                  return (
-                    <td>
-                      <div
-                        className={`col col${i + 1} ${
-                          this.state.click[5] ? "open1" : null
-                        } ${this.state.test[i] ? "open2" : null}`}
-                        onClick={() => {
-                          this.clickRow(5);
-                          this.clickColumn(i);
-                        }}
-                      >
-                        <div
-                          className={`${ele.cheapest ? "cheapest" : ""}`}
-                        ></div>
-                        <span>{ele.price}</span>
-                      </div>
-                    </td>
-                  );
-                })}
-              </div>
-            </tr>
-            <tr>
-              <td>
-                <div className="date">
-                  <span>01/02(二)</span>
-                </div>
-              </td>
-              <div className={`slide clearfix ${column}`}>
-                {JsonData[6].detail.map((ele, i) => {
-                  {
-                    /* JsonData[0].detail */
-                  }
-                  // {JsonData.detail[0].price.map((ele, i) => {
-                  return (
-                    <td>
-                      <div
-                        className={`col col${i + 1} ${
-                          this.state.click[6] ? "open1" : null
-                        } ${this.state.test[i] ? "open2" : null}`}
-                        onClick={() => {
-                          this.clickRow(6);
-                          this.clickColumn(i);
-                        }}
-                      >
-                        <div
-                          className={`${ele.cheapest ? "cheapest" : ""}`}
-                        ></div>
-                        <span>{ele.price}</span>
-                      </div>
-                    </td>
-                  );
-                })}
-              </div>
-            </tr>
+                  </tr>
+                );
+              })}
+            </tbody>
           </table>
         </div>
       </React.Fragment>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Date from "./components/dataInfo.json";
 import Table from "./components/table";
 import "./components/index.scss";
 
 class Fztable extends React.Component {
   componentDidMount() {}
   render() {
+    const props = {
+      count: {
+        // M版時每次點擊往前往後移動幾格儲存格
+        slide: 1, // [number]
+        // M版時一個畫面show幾格儲存格
+        show: 4 // [number]
+      },
+      // 設定花多久時間移動完成
+      speed: 0.3, // [number]
+      // 每次點擊儲存格時會執行此callback，並帶入所點擊的儲存格jquery物件
+      whenClick: function($element) {
+        // console.log($element)
+      }
+    };
+
     return (
       <React.Fragment>
         <div className="colume">
